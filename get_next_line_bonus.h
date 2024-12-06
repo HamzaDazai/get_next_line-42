1f285b66a413cc33cab7ee81bd45ff86f9295279
hamza Dazia <135356014+HamzaDazai@users.noreply.github.com>
2024-12-06 23:46:42 +0100

    Update get_next_line_bonus.h

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hdazia <hdazia@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/01 08:27:21 by hdazia            #+#    #+#             */
-/*   Updated: 2024/12/03 20:16:40 by hdazia           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef GET_NEXT_LINE_BONUS_H
 # define GET_NEXT_LINE_BONUS_H
 
@@ -22,12 +10,13 @@
 
 char	*get_next_line(int fd);
 char	*ft_strdup(const char *s1);
-char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strjoin(char *s1, char *s2);
 size_t	ft_strlen(const char *str);
 char	*ft_substr(const char *s, unsigned int start, size_t len);
 char	*ft_stored_string(char **str, int place_nline);
-char	*ft_get_line(char **storing, char **line, int place_nline);
-int		ft_find_newline(const char *str);
+char	*ft_get(char **storing, int place_nline);
+int		ft_find(const char *str);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+void	*check_null(char **buffer, char **storing);
 
 #endif
