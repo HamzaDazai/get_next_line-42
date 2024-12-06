2b71f93c3eaaec14279863385f9df3a2261d6919
hamza Dazia <135356014+HamzaDazai@users.noreply.github.com>
2024-12-06 23:47:45 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -16,7 +16,6 @@ char	*ft_substr(const char *s, unsigned int start, size_t len);
 char	*ft_stored_string(char **str, int place_nline);
 char	*ft_get(char **storing, int place_nline);
 int		ft_find(const char *str);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 void	*check_null(char **buffer, char **storing);
 
 #endif
